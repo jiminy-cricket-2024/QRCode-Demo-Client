c792d7e64d1c9731afbef0a3a4bdfa41a37e5e2b
MuhammadUmar00 <darazkhan250@gmail.com>
2024-09-10 23:16:49 +0400

    changed droplet url

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-let clientUrl = "http://23.21.71.157:3001";
-let baseUrl = "http://23.21.71.157:5000";
+let clientUrl = "http://134.209.124.223:3001";
+let baseUrl = "http://134.209.124.223:5000";
 
 export { baseUrl, clientUrl };
